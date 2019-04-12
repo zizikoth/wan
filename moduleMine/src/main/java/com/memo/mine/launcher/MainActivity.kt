4d2cd5982b65074f32ed3cb23803d74769314ac3
memo <zhou_android@163.com>
2019-04-13 00:39:33 +0800

    导包

--- moduleMine/src/main/java/com/memo/mine/launcher/MainActivity.kt
+++ moduleMine/src/main/java/com/memo/mine/launcher/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.memo.mine.launcher
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.memo.mine.R
 
+/**
+ * title:单独运行时的主界面
+ * describe:
+ *
+ * @author zhou
+ * @date 2019-04-11 00:17
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
